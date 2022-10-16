0ffa1de7f509f94769bc08a5141712f0fbf541fe
Siyu Wang <siw069@ucsd.edu>
2022-10-16 16:47:51 -0400

    start

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
-app = Flask(__name__)
+from azure.cosmos import CosmosClient
+import json
+import os
 
+
+endpoint = "https://ml2499-db.documents.azure.com:443/"
+key = "eENzBybN77GqV9gVlPsb9IfmiKTHJIX9q4zMF7K0Em36T3S2Xr8zAlcsODo6N6XP1808AY3Hto0vGmoKLcjRZw=="
+client = CosmosClient(endpoint, credential = key)
+database_name = 'Tasks'
+container_name = 'Container1'
+database_obj  = client.get_database_client(database_name)
+container = database_obj.get_container_client(container_name)
+
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-   print('Request for index page received')
-   return render_template('index.html')
+    print('Request for index page received')
+    return render_template('index.html')
 
 @app.route('/favicon.ico')
 def favicon():
@@ -15,15 +27,15 @@ def favicon():
 
 @app.route('/hello', methods=['POST'])
 def hello():
-   name = request.form.get('name')
-
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
-
+    origin = request.form.get('origin')
+    dest = request.form.get('dest')
+    for item in container.query_items(query='SELECT * FROM Container1 i', enable_cross_partition_query=True):
+        print(json.dumps(item, indent=True))
+    if origin and dest:
+        return render_template('hello.html', origin = origin, dest = dest)
+    else:
+        print('Request for hello page received with no name or blank name -- redirecting')
+        return redirect(url_for('index'))
 
 if __name__ == '__main__':
    app.run()
